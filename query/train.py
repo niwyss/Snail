4015d47e4db84fc55d83a5cba2bbf19f3f2544d8
Nicolas Wyss <nicolaswyss@gmail.com>
2012-07-05 23:10:11 +0200

    Correction sur la gestion des path

--- query/train.py
+++ query/train.py
@@ -5,14 +5,14 @@ import sys
 import httplib
 import sqlite3
 
-def list(database_path, station_code_ddg):
+def list(database_path, parameters_path, station_code_ddg):
 
     # Connection to the base
     connection = sqlite3.connect(database_path)
     connection.row_factory = sqlite3.Row
     
     # Request parameters
-    data = open('parameters.json', 'r')
+    data = open(parameters_path, 'r')
     json_data_parametres = json.loads(data.read())
     data.close()
     HOST = json_data_parametres["host"] 
@@ -27,23 +27,24 @@ def list(database_path, station_code_ddg):
         code = station['codeDDG']
         name = station['name']
         stations[code] = name
+    
+    # Only if code exits in database
+    if station_code_ddg in stations:
 
-    # Get informations for this station 
-    conn = httplib.HTTPConnection(HOST)
-    conn.request("POST", SELECTOR, PARAMS % station_code_ddg, HEADERS)
-    response = conn.getresponse()
-    data = response.read()
-    conn.close()
-    json_data_trains = json.loads(data)
-
-    # Print list of next trains for this station
-    trains = json_data_trains[0]['data']
-    for train in trains:
-        code = train['trainMissionCode']
-        terminus = train['trainTerminus'].strip().encode("utf-8", 'replace')
-        lane = train['trainLane']
-        hour = train['trainHour'][11:]
-        name = stations[terminus].strip().encode("utf-8", 'replace')
-        print('{0:4}  {1:5}  {3:1}  {2:35}'.format(code, hour, name, lane))
-
-
+        # Get informations for this station 
+        conn = httplib.HTTPConnection(HOST)
+        conn.request("POST", SELECTOR, PARAMS % station_code_ddg, HEADERS)
+        response = conn.getresponse()
+        data = response.read()
+        conn.close()
+        json_data_trains = json.loads(data)
+        
+        # Print list of next trains for this station
+        trains = json_data_trains[0]['data']
+        for train in trains:
+            code = train['trainMissionCode']
+            terminus = train['trainTerminus'].strip().encode("utf-8", 'replace')
+            lane = train['trainLane']
+            hour = train['trainHour'][11:]
+            name = stations[terminus].strip().encode("utf-8", 'replace')
+            print('{0:4}  {1:5}  {3:1}  {2:35}'.format(code, hour, name, lane))
