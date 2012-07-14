5b832708386c5cb5de6c833188948bd78bd00b39
Nicolas Wyss <nicolaswyss@gmail.com>
2012-07-14 18:23:08 +0200

    Recherche de plusieurs stations par nom

--- query/station.py
+++ query/station.py
@@ -5,7 +5,27 @@ import sys
 import sqlite3
 import os
 
-def list(database_path, name):
+def value_to_arg(value): 
+    return '%' + value + '%'
+
+def fetch_stations_by_name(connection, names):
+    if names and len(names) != 0:
+        sql = ' SELECT * FROM station WHERE ' + ' OR '.join('name LIKE ?' for n in names)
+    else:
+        sql = ' SELECT * FROM station '
+    args = map(value_to_arg, names)
+    return connection.execute(sql, args).fetchall()
+
+def print_station(station):
+    codeDDG = station['codeDDG']
+    codeQLT = station['codeQLT']
+    codeUIC = station['codeUIC']
+    longitude = station['longitude']
+    latitude = station['latitude']
+    name = station['name'].strip().encode("utf-8", 'replace')
+    print('{0:3}  {1:35} '.format(codeDDG, name))
+
+def list(database_path, names):
 
     # Test : database
     if not os.path.exists(database_path):
@@ -16,11 +36,10 @@ def list(database_path, name):
     connection = sqlite3.connect(database_path)
     connection.row_factory = sqlite3.Row
 
-    # Fetch data
-    sql = ' SELECT * FROM station where name like ? '
-    args = ['%' + name + '%']
-    for station in connection.execute(sql, args):
-        codeDDG = station['codeDDG']
-        name = station['name'].strip().encode("utf-8", 'replace')
-        print('{0:3}  {1:35} '.format(codeDDG, name))
+    # Print stations
+    for station in fetch_stations_by_name(connection, names):
+        print_station(station)
+
+    # Close the connection
+    connection.close()
 
