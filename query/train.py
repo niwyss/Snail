c8a171368062f72978b75e6f712f9411bc157fa1
Nicolas Wyss <nicolaswyss@gmail.com>
2012-07-15 17:56:08 +0200

    Préparation gestion multi web-services

--- query/train.py
+++ query/train.py
@@ -24,7 +24,7 @@ import httplib
 import sqlite3
 import os
 
-def fetch_trains(host, selector, params, headers, code_station):
+def __fetch_trains(host, selector, params, headers, code_station):
     try:
         conn = httplib.HTTPConnection(host)
         conn.request("POST", selector, params % code_station, headers)
@@ -36,16 +36,16 @@ def fetch_trains(host, selector, params, headers, code_station):
         print "snail: error: web service unreachable."
         sys.exit(0)
 
-def fetch_all_stations(connection):
+def __fetch_all_stations(connection):
     sql = ' SELECT * FROM station '
     return connection.execute(sql).fetchall()
 
-def fetch_station_by_code_ddg(connection, code_ddg):
+def __fetch_station_by_code_ddg(connection, code_ddg):
     sql = ' SELECT * FROM station where codeDDG = ? '
     args = [code_ddg]
     return connection.execute(sql, args).fetchone()
 
-def print_station(station):
+def __print_station(station):
     name = station['name'].strip().encode("utf-8", 'replace')
     codeDDG = station['codeDDG']
     codeQLT = station['codeQLT']
@@ -54,7 +54,7 @@ def print_station(station):
     latitude = station['latitude']
     print("{0} [{1}] ({2}/{3})").format(name, codeDDG, longitude, latitude)
 
-def print_train(train, stations):
+def __print_train(train, stations):
     code = train['trainMissionCode']
     terminus = train['trainTerminus'].strip().encode("utf-8", 'replace')
     lane = train['trainLane']
@@ -63,7 +63,7 @@ def print_train(train, stations):
     name = stations[terminus].strip().encode("utf-8", 'replace')
     print('{0:5}  {1:4}  {4:6}  {3:1}  {5:3}  {2:35}'.format(hour, code, name, lane, number, terminus))
 
-def print_information(information):
+def __print_information(information):
     print " - " + information.strip().encode("utf-8", 'replace').replace("\n", "\n   ")
   
 def list(database_path, parameters_path, station_code_ddg):
@@ -89,7 +89,7 @@ def list(database_path, parameters_path, station_code_ddg):
       
     # Transforms stations list into dictionnary
     stations = {}
-    for station in fetch_all_stations(connection):
+    for station in __fetch_all_stations(connection):
         code = station['codeDDG']
         name = station['name']
         stations[code] = name
@@ -100,27 +100,27 @@ def list(database_path, parameters_path, station_code_ddg):
         # Get informations for this station 
         host = json_data_parametres["host"] 
         selector = json_data_parametres["selector"] 
-        params = json_data_parametres["params"]
+        params = json_data_parametres["params"]["list_trains"]
         headers = json_data_parametres["headers"]
-        json_data_trains = fetch_trains(host, selector, params, headers, station_code_ddg)
+        json_data_trains = __fetch_trains(host, selector, params, headers, station_code_ddg)
         
         # Print informations of the station
-        station = fetch_station_by_code_ddg(connection, station_code_ddg)
-        print_station(station)
+        station = __fetch_station_by_code_ddg(connection, station_code_ddg)
+        __print_station(station)
 
         # Print list of next trains for this station
         trains = json_data_trains[0]['data']
         if trains and not len(trains) == 0:
             print "\n%d train(s)" % len(trains)
             for train in trains:
-               print_train(train, stations)
+               __print_train(train, stations)
 
         # Print informations on the track
         infos = json_data_trains[0]['list']
         if infos and not len(infos) == 0:
             print "\n%d information(s)" % len(infos)
             for information in infos:
-                print_information(information)
+                __print_information(information)
 
     # Close the connection
     connection.close()
